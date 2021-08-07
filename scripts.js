81c0d97642b99eafa875e04d6554c3f177615b34
chesino <45505217+chesino@users.noreply.github.com>
2021-08-07 23:04:39 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -41,9 +41,7 @@ function openPage() {
       window.open("/300801.html");
   }
 
-  if (x === "TCN") {
-      window.open("/191204.html");
-  }
+
 
   if (x === "HUY123") {
     window.open("/gif001.html");
@@ -57,7 +55,7 @@ function server2() {
 }
 
 function servervip() {
-  if (window.confirm('Bạn không thể truy cập máy chủ này. \nĐây là máy chủ dành riêng cho thành viên trả phí \nNhấn "OK" để mua thành viên trả phí')) 
+  if (window.confirm('Máy chủ hoạt động ổn định. \nDịch vụ: 15/15 \nNhấn "OK" để mua thành viên trả phí')) 
 {
 window.location.href='https://m.me/hunqD';
 };
@@ -65,29 +63,4 @@ window.location.href='https://m.me/hunqD';
 
 
 
-var time = 30;
-var page = "https://docs.google.com/forms/d/1zhn6z1mjn3a-wl_SFSnz04u6g8Vnj6u7iBtNcuI0stI/viewform?edit_requested=true";
 
-function countDown(){
-time--;
-gett("container").innerHTML = time;
-if(time == 0){
-window.location = page;
-}
-}
-function gett(id){
-if(document.getElementById) return document.getElementById(id);
-if(document.all) return document.all.id;
-if(document.layers) return document.layers.id;
-if(window.opera) return window.opera.id;
-}
-function init(){
-if(gett('container')){
-setInterval(countDown, 1000);
-gett("container").innerHTML = time;
-}
-else{
-setTimeout(init, 0);
-}
-}
-document.onload = init();
