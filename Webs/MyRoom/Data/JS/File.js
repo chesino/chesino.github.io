90e0c9791afb0150eef852a47210abfcc257b9d5
Chesino <45505217+chesino@users.noreply.github.com>
2022-12-24 03:24:33 +0700

    Update File.js

--- Webs/MyRoom/Data/JS/File.js
+++ Webs/MyRoom/Data/JS/File.js
@@ -1,7 +1,7 @@
-// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//   document.body.style.backgroundColor = '#111' ;
-//   document.body.style.color = '#eee' ;
-// }
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.style.backgroundColor = '#111' ; 
+  document.body.style.color = '#eee' ;
+ }
 
 
 
