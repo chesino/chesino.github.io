0e1621c0894084e69efb6b6be1830a18d16e3722
Chesino <45505217+chesino@users.noreply.github.com>
2023-02-24 12:33:56 +0700

    HunqD

--- Data/JS/File.js
+++ Data/JS/File.js
@@ -96,3 +96,47 @@ function mode3() {
 
 
 
+var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+
+if (isIOS) {
+
+    var canvasVideo = new CanvasVideoPlayer({
+        videoSelector: '.video',
+        canvasSelector: '.canvas',
+        timelineSelector: false,
+        autoplay: true,
+        makeLoop: true,
+        pauseOnClick: false,
+        audio: false
+    });
+
+}else {
+
+    // Use HTML5 video
+    document.querySelectorAll('.canvas')[0].style.display = 'none';
+
+} 
+
+
+
+// Menu 
+function openTab(evt, tabName) {
+  var i, page, Function;
+  page = document.getElementsByClassName("Page");
+
+
+  Function = document.getElementsByClassName("Function");
+  for (i = 0; i < Function.length; i++) {
+    Function[i].className = Function[i].className.replace(" Active", "");
+  }
+
+  const targetSection = document.getElementById(tabName);
+  const targetOffsetTop = targetSection.offsetTop;
+  window.scrollTo({
+    top: targetOffsetTop,
+    behavior: "smooth"
+  });
+
+  evt.currentTarget.className += " Active";
+}
+
