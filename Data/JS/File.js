2833489f590de5ee22c04054d4b37c1233b4e058
Chesino <45505217+chesino@users.noreply.github.com>
2023-02-24 13:48:01 +0700

    HunqD

--- Data/JS/File.js
+++ Data/JS/File.js
@@ -96,27 +96,6 @@ function mode3() {
 
 
 
-var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
-
-if (isIOS) {
-
-    var canvasVideo = new CanvasVideoPlayer({
-        videoSelector: '.video',
-        canvasSelector: '.canvas',
-        timelineSelector: false,
-        autoplay: true,
-        makeLoop: true,
-        pauseOnClick: false,
-        audio: false
-    });
-
-}else {
-
-    // Use HTML5 video
-    document.querySelectorAll('.canvas')[0].style.display = 'none';
-
-} 
-
 
 
 // Menu 
