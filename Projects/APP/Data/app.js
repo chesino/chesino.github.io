bc17626ce3fa3e657cc3c8d37e8a2b0e5aba4b1e
Chesino <45505217+chesino@users.noreply.github.com>
2023-03-26 22:32:55 +0700

    HunqD

---
+++ Projects/APP/Data/app.js
@@ -0,0 +1,90 @@
+const apps = [
+  {
+    type: "Game",
+    logo: "https://is5-ssl.mzstatic.com/image/thumb/Purple123/v4/86/53/e8/8653e877-4280-0afd-1bb3-0d50a81ce13f/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/217x0w.webp",
+    name: "Farm War Empires",
+    os: "iOS",
+    description: "Nhiều Vàng",
+    downloadLink: ""
+  },
+  {
+    type: "App",
+    logo: "https://is5-ssl.mzstatic.com/image/thumb/Purple116/v4/6f/98/9d/6f989d2c-fe62-0cb4-452d-0a64e6e8207e/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/217x0w.webp",
+    name: "Sách hay",
+    os: "iOS",
+    description: "Đã mở khoá VIP",
+    downloadLink: ""
+  },
+  {
+    type: "Game",
+    logo: "https://is5-ssl.mzstatic.com/image/thumb/Purple126/v4/30/fc/d9/30fcd9d9-d49c-e144-ccc7-335aaa3f54db/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/217x0w.webp",
+    name: "Adorimon",
+    os: "iOS",
+    description: "Nhiều tiền",
+    downloadLink: ""
+  },
+  {
+    type: "Game",
+    logo: "https://is4-ssl.mzstatic.com/image/thumb/Purple116/v4/5c/32/5b/5c325b9f-87f3-d806-09ae-c027ee82dde6/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/350x350.png?",
+    name: "Flashback",
+    os: "iOS",
+    description: "Hack",
+    downloadLink: ""
+  },
+  {
+    type: "Game",
+    logo: "https://is2-ssl.mzstatic.com/image/thumb/Purple116/v4/b7/9e/a3/b79ea34f-1cc6-882f-cee5-127c7f899baa/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/350x350.png?",
+    name: "Impossible Date: Tricky Riddle",
+    os: "iOS",
+    description: "Hack",
+    downloadLink: ""
+  },
+  {
+    type: "Game",
+    logo: "https://is4-ssl.mzstatic.com/image/thumb/Purple116/v4/41/8e/c7/418ec78b-f22e-ef5a-d0ce-11f359514c35/AppIcon-0-0-1x_U007emarketing-0-0-0-4-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/350x350.png?",
+    name: "Hero Castle Wars",
+    os: "iOS",
+    description: "Hack",
+    downloadLink: ""
+  },
+  {
+    type: "App",
+    logo: "https://is3-ssl.mzstatic.com/image/thumb/Purple126/v4/52/91/ec/5291ec48-e2f9-13be-8d1c-1c6907291701/AppIcon-0-0-1x_U007emarketing-0-0-0-6-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/350x350.png?",
+    name: "iAntiTheft",
+    os: "iOS",
+    description: "Mở khoá PRO",
+    downloadLink: ""
+  },
+  {
+    "type": "Game",
+    "logo": "https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/7c/5c/57/7c5c57e0-3044-8346-7c70-a5f69ece12fc/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/350x350.png?",
+    "name": "Brain Out -Tricky riddle games",
+    "os": "iOS",
+    "description": "Hack",
+    "downloadLink": ""
+  },
+  {
+    "type": "Game",
+    "logo": "https://is5-ssl.mzstatic.com/image/thumb/Purple122/v4/45/85/1b/45851bff-2f3d-5546-e1dc-d833ae8a04c6/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/350x350.png?",
+    "name": "Banana Kong",
+    "os": "iOS",
+    "description": "Mở khoá, Vô vạn tiền",
+    "downloadLink": ""
+  },
+  {
+    "type": "Game",
+    "logo": "https://is4-ssl.mzstatic.com/image/thumb/Purple122/v4/7b/37/7f/7b377f18-84ba-8a17-0d95-ac3c52b889b7/AppIcon-1x_U007emarketing-0-7-0-85-220.png/350x350.png?",
+    "name": "Banana Kong 2",
+    "os": "iOS",
+    "description": "Mở khoá, Vô vạn tiền",
+    "downloadLink": ""
+  },
+  {
+    "type": "App",
+    "logo": "https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/66/87/40/668740b1-3b86-0405-2d68-de7781686365/AppIcon-1x_U007emarketing-0-10-0-85-220.png/350x350.png?",
+    "name": "Friendly Plus Social Browser",
+    "os": "iOS",
+    "description": "Mở khoá gói Plus",
+    "downloadLink": ""
+  }
+];
---
+++ Projects/APP/Upload/script.js
@@ -0,0 +1,45 @@
+function submitForm() {
+    const type = document.getElementById("type").value;
+    const logo = document.getElementById("logo").value;
+    const name = document.getElementById("name").value;
+    const os = document.getElementById("os").value;
+    const description = document.getElementById("description").value;
+    const downloadLink = document.getElementById("downloadLink").value;
+
+    const data = {
+        type: type,
+        logo: logo,
+        name: name,
+        os: os,
+        description: description,
+        downloadLink: downloadLink
+    };
+
+    const output = document.getElementById("output");
+    output.textContent = JSON.stringify(data, null, 2);
+}
+const copyBtn = document.getElementById('copyBtn');
+
+copyBtn.addEventListener('click', () => {
+  // Lấy các giá trị của các trường form
+  const typeValue = document.getElementById('type').value;
+  const logoValue = document.getElementById('logo').value;
+  const nameValue = document.getElementById('name').value;
+  const osValue = document.getElementById('os').value;
+  const descriptionValue = document.getElementById('description').value;
+  const downloadLinkValue = document.getElementById('downloadLink').value;
+
+  // Tạo dữ liệu định dạng JSON từ các giá trị đã lấy được
+  const data = {
+    type: typeValue,
+    logo: logoValue,
+    name: nameValue,
+    os: osValue,
+    description: descriptionValue,
+    downloadLink: downloadLinkValue
+  };
+
+  // Chuyển dữ liệu sang định dạng JSON và sao chép vào clipboard
+  const jsonData = JSON.stringify(data, null, 2);
+  navigator.clipboard.writeText(jsonData);
+});
