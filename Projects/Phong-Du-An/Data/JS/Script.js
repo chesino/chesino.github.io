36beb5cb247f8ae048664938654e94500f2f086b
Chesino <45505217+chesino@users.noreply.github.com>
2023-04-12 22:21:53 +0700

    HunqD

--- Projects/Phong-Du-An/Data/JS/Script.js
+++ Projects/Phong-Du-An/Data/JS/Script.js
@@ -1,3 +1,17 @@
 setTimeout(function() {
-    document.querySelector('.loading').classList.add('hidden');
-  }, 3000); // 3 giây = 3000ms
+    document.getElementById('loading').classList.add('hidden');
+    setTimeout(() => {
+      document.getElementById('loading').style.display = 'none';
+    }, 1200);
+  }, 1000);
+
+  const progressBar = document.querySelector('.progress-bar');
+  var ValueProgress = document.getElementById('ValueProgress');
+  function updateProgress(progress, color) {
+    progressBar.style.width = `${progress}%`;
+    progressBar.setAttribute('aria-valuenow', progress);
+    ValueProgress.innerHTML = `${progress}%`;
+    progressBar.classList.remove('blue', 'orange', 'red', 'green'); 
+    progressBar.classList.add(color); // Thêm lớp màu sắc mới
+  }
+  updateProgress(0)
