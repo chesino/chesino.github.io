0b1f02ea7bab29df86df17ebb84acbbecc32cc1c
Chesino <bidinh219@gmail.com>
2023-05-30 21:28:57 +0700

    HunqD

--- Data/JS/File.js
+++ Data/JS/File.js
@@ -1,96 +1,118 @@
-// Kiểm tra xem trình duyệt có phải là trình duyệt trên điện thoại không
-var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-if (isMobile) {
-  console.log("Đây là trình duyệt trên điện thoại");
-} else {
-  console.log("Đây là trình duyệt trên máy tính");
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#iDarkMode');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+  document.getElementById("dark-mode-toggle").className = "fa-solid fa-moon";
+
 }
 
-
-// Lấy nút nhấn và thêm sự kiện nhấn vào
-const button = document.getElementById('toggle-button');
-button.addEventListener('click', toggleMode);
-
-// Khởi tạo chế độ mặc định và lưu vào local storage (nếu chưa có)
-let mode = parseInt(localStorage.getItem('mode')) || 1;
-setMode(mode);
-
-// Hàm chuyển đổi chế độ
-function toggleMode() {
-  // Tăng chế độ lên 1 và quay về chế độ 1 nếu chế độ hiện tại là 3
-  mode = mode < 3 ? mode + 1 : 1;
-  setMode(mode);
-  // Lưu chế độ vào local storage
-  localStorage.setItem('mode', mode);
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+  document.getElementById("dark-mode-toggle").className = "fa-solid fa-sun";
 }
 
-// Hàm thiết lập chế độ
-function setMode(mode) {
-  switch (mode) {
-    case 1:
-      button.innerHTML = '<i class="fa-regular fa-sun"></i>';
-      button.style.backgroundColor = '#40c3da';
-      button.style.color = '#000';
-      // Chạy hàm cho chế độ 1
-      mode1();
-      break;
-    case 2:
-      button.innerHTML = '<i class="fa-regular fa-moon"></i>';
-      button.style.backgroundColor = '#211e2c';
-      button.style.color = '#eee';
-
-      // Chạy hàm cho chế độ 2
-      mode2();
-      break;
-    case 3:
-
-      button.style.backgroundColor = '#f5c85d';
-      button.style.color = '#000';
-
-      if (isMobile) {
-        button.innerHTML = '<i class="fa-solid fa-mobile-screen-button"></i>';
-      } else {
-        button.innerHTML = '<i class="fa-solid fa-desktop"></i>';
-      }
-
-
-      // Chạy hàm cho chế độ 3
-      mode3();
-      break;
-    default:
-      break;
-  }
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
 }
 
-// Hàm cho chế độ 1 chế độ sáng
-function mode1() {
-  console.log('Chế độ 1');
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode');
 
-  var body = document.querySelector('body');
-  body.classList.remove('dark-mode');
-}
-
-// Hàm cho chế độ 2 chế độ tối
-function mode2() {
-  console.log('Chế độ 2');
-
-  var body = document.querySelector('body');
-  body.classList.add('dark-mode');
-}
-
-// Hàm cho chế độ 3 chế độ sáng / tối
-function mode3() {
-  console.log('Chế độ 3');
-
-  var body = document.querySelector('body');
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    body.classList.add('dark-mode');
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    // if it has been enabled, turn it off  
   } else {
-    body.classList.remove('dark-mode');
+    disableDarkMode();
+  }
+});
+
+window.onscroll = function () { myBar() };
+
+function myBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  var Head = document.getElementById("Head");
+  var SpaceMenu = document.getElementById("SpaceMenu");
+  var idProgress = document.getElementById("idProgress");
+  
+  document.getElementById("myBar").style.width = scrolled  + "%";
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 220) {
+    SpaceMenu.style.display = "block";
+    idProgress.style.display = 'block';
+    Head.style.position = "fixed";
+    Head.style.backdropFilter = "blur(60px)";
+    Head.style.backgroundColor = "var(--backgroundMenu)";
+  }
+  else {
+    SpaceMenu.style.display = "none";
+    idProgress.style.display = 'none';
+    Head.style.position = "relative";
+    document.getElementById("Head").style.backdropFilter = "blur(0px)";
+    document.getElementById("Head").style.backgroundColor = "transparent";
+    Head.style.padding = "10px 5px 15px 15px";
+    
   }
 }
 
+$('#LogoHead').click(function () {
+  setTimeout(function () {
+    $('html,body').animate({
+      scrollTop: '0'
+    },
+      'slow');
+
+  }, 100);
+});
+
+function NavMenu() {
+  $("body").css("overflow","hidden");
+  var NavCheck = document.getElementById('NavCheck');
+  if (NavCheck.className == 'fa-solid fa-bars-staggered') {
+    NavCheck.className = 'fa-solid fa-x'
+    openNav();
+  } else {
+    $("body").css("overflow","auto");
+    NavCheck.className = 'fa-solid fa-bars-staggered'
+    closeNav();
+  }
+}
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("Head").style.backgroundColor = "var(--backgroundBody)";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("Head").style.backgroundColor = "transparent";}
 
 
-
+function openCategory(evt, Category) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Category).style.display = "block";
+  evt.currentTarget.className += " active";
+}
