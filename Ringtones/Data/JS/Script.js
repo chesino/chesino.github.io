6997c65e773f41fbd4bd0f6779ccfcd4f1c33fa0
Chesino <117713475+HunqDxSwBun@users.noreply.github.com>
2023-07-17 20:54:00 +0700

    H

--- Ringtones/Data/JS/Script.js
+++ Ringtones/Data/JS/Script.js
@@ -8,52 +8,51 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-let audioSource = audioCtx.createMediaElementSource(audio1);
-let analyser = audioCtx.createAnalyser();
-
-audioSource.connect(analyser);
-analyser.connect(audioCtx.destination);
-analyser.fftSize = 128 * 8;
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
-const barWidth = canvas.width / bufferLength * 2;
-let x = 0;
+let audioSource = null;
+let analyser = null;
 
 function playAudio() {
     audio1.play();
-}
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    analyser = audioCtx.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    analyser.fftSize = 128 * 16;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    const barWidth = canvas.width / bufferLength * 2;
+    let x = 0;
 
-function animate() {
-    x = 0;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    analyser.getByteFrequencyData(dataArray);
-    drawVisualizer({
+    function animate() {
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        drawVisualizer({
+            bufferLength,
+            dataArray,
+            barWidth
+        });
+        requestAnimationFrame(animate);
+    }
+
+    const drawVisualizer = ({
         bufferLength,
         dataArray,
         barWidth
-    });
-    requestAnimationFrame(animate);
-}
-let hue = 0;
-const hueIncrement = 0; // Giá trị tăng hue chậm hơn
-
-function drawVisualizer({ bufferLength, dataArray, barWidth }) {
-    let barHeight;
-    for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-        
-        const color = `hsl(${hue}, 100%, 50%)`;
-        ctx.fillStyle = color;
-
-        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-        x += barWidth;
-
-        hue += hueIncrement; // Tăng giá trị hue chậm hơn
+    }) => {
+        let barHeight;
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            const red = (i * barHeight);
+            const green = i;
+            const blue = barHeight;
+            ctx.fillStyle = `rgb(${green}, ${blue}, ${red})`;
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth;
+        }
     }
+
+    animate();
 }
-
-
-
-animate();
 
 container.addEventListener("click", playAudio);
