3abb2e48f2da644bf31b09e5421d399199ae4d08
HunqDxSwBun <117713475+HunqDxSwBun@users.noreply.github.com>
2023-09-05 17:06:34 +0700

    HunqD

--- Projects/TKB/Data/JS/File.js
+++
@@ -1,7 +0,0 @@
-const table = document.querySelector('table');
-const today = new Date().getDay();
-
-if (today > 0 && today < 7) {
-    table.rows[today].classList.add('today');
-    table.rows[today].cells[3].style.backgroundColor = 'red';
-}
