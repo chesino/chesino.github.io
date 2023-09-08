3093543c8528accb938e8fc96833ea92df67f82e
HunqDxSwBun <117713475+HunqDxSwBun@users.noreply.github.com>
2023-09-08 18:06:38 +0700

    HunqD

--- DATA/Script.js
+++ DATA/Script.js
@@ -13,7 +13,7 @@ function myFunction() {
 }
 
 
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -23,6 +23,90 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  let formattedTime = '';
+
+  if (hours > 0) {
+    formattedTime += hours + ' Giờ ';
+  }
+  if (minutes > 0 || hours > 0) {
+    formattedTime += minutes + 'Phút ';
+  }
+  formattedTime += remainingSeconds + 'Giây';
+
+  return formattedTime;
+}
+function playMedia() {
+  const input = document.getElementById('IDMedia').value;
+  const mediaType = document.getElementById('mediaType').value;
+  const mediaContainer = document.getElementById('Player');
+  const Media = document.getElementById('Media');
+  let driveId = input; // Mặc định là input đầu vào
+
+  // Kiểm tra nếu input là một URL hợp lệ
+  const urlMatch = input.match(/drive\.google\.com\/(file\/d\/|open\?id=)([^&]+)/);
+  if (urlMatch) {
+      driveId = urlMatch[2];
+  }
+
+  const url = `https://drive.google.com/uc?export=download&id=${driveId}`;
+
+  let mediaElement;
+
+  if (mediaType === 'video') {
+      mediaElement = document.createElement('video');
+      Media.classList.add("Dark")
+      setTimeout(() => {
+        mediaElement.play();
+      }, 1000);
+  } else if (mediaType === 'audio') {
+      mediaElement = document.createElement('audio');
+      Media.classList.remove("Dark");
+  }
+
+  if (mediaElement) {
+      mediaElement.src = url;
+      mediaElement.controls = true;
+      mediaElement.addEventListener('loadedmetadata', () => {
+          // Metadata đã được nạp, kiểm tra thông tin tệp
+          const durationInSeconds = mediaElement.duration;
+
+          // Chuyển đổi độ dài thành dạng giờ, phút và giây
+          const durationFormatted = formatTime(durationInSeconds);
+
+          let resolution = '';
+          if (mediaType === 'video') {
+              resolution = `${mediaElement.videoWidth}x${mediaElement.videoHeight}`;
+
+              // Thêm thông tin độ phân giải ví dụ (480p)
+              if (mediaElement.videoHeight === 480) {
+                  resolution += ' (480p)';
+              }
+          }
+          // Hiển thị thông tin tệp
+          mediaContainer.innerHTML = `
+          <div class="Type">
+              <p>ID: ${driveId}</p>
+              <p>Thời gian: ${durationFormatted}</p>
+              <p>Độ phân giải: ${resolution}</p>
+          </div>
+          `;
+
+          // Thêm mediaElement vào mediaContainer
+          mediaContainer.appendChild(mediaElement);
+      });
+
+      mediaElement.addEventListener('error', (error) => {
+          console.error('Lỗi khi tải tệp:', error);
+          mediaContainer.innerHTML = 'Không thể tải tệp.';
+      });
+  }
 }
