a2dc803aca0beaeb95f34eba5de1e36b1443ba34
Chesino <45505217+chesino@users.noreply.github.com>
2023-11-21 18:53:59 +0700

    HunqD

--- Apps/Apps/Data/app.js
+++ Apps/Apps/Data/app.js
@@ -4,6 +4,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/a0/64/61/a06461f8-3ab4-e1a5-38cb-679041ec9be5/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp",
     "name": "Sách hay",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "1,2MB",
     "description": "Premiun vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=14RIkAGdhtH_cJyIUrf6K4GIA3Baa_xo-"
   },
@@ -12,14 +14,18 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/ec/a5/3f/eca53f0d-1843-4aa3-a061-72634d33bc6f/AppIcon-0-1x_U007emarketing-0-6-0-0-85-220-0.png/246x0w.webp",
     "name": "Zip & RAR File Extractor",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "303KB",
     "description": "Premiun vĩnh viễn",
-    "downloadLink": "https://drive.google.com/uc?export=download&id=1O3IGQS0sKg_dXc7swi5Pq_Gd8dT9wEw9"
+    "downloadLink": "https://drive.google.com/uc?export=download&id=1rGPLedFtV5VvPrpAgWFmRlCh3mlJ9qLI"
   },
   {
     "type": "App",
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/41/e1/68/41e16822-7a1a-d3c4-ca64-5f40e7ce58dd/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp",
     "name": "Migii JLPT ",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "50,3MB",
     "description": "Premium 1 Năm (Hạn: 18/11/2024)",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1Qds6_IMUZCvN1c42ihPCy72rscPKkz0G"
   },
@@ -28,6 +34,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple125/v4/be/07/22/be0722b0-8162-dcc2-3db1-ef9e57c07595/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp",
     "name": "JLPT N5 ~ N1 Learn Japanese",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "7,9MB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1oMIlVxX4HRbSSnYnlqWsSnjtbAF4-Zix"
   },
@@ -36,6 +44,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/48/90/e5/4890e5b1-af4c-1231-a3d9-2a0d8dbd92ab/AppIcon-1x_U007emarketing-0-10-0-sRGB-85-220.png/246x0w.webp",
     "name": "Sổ thu chi",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "418KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1JQKCP-BEKSolUWXIr93CgSmeRyfXFLzA"
   },
@@ -44,6 +54,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/52/91/ec/5291ec48-e2f9-13be-8d1c-1c6907291701/AppIcon-0-0-1x_U007emarketing-0-0-0-6-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp",
     "name": "iAntiTheft",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "28KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1m6Yp8yjOlUiWuoYAubz0IyF5Fi5u1WhP"
   },
@@ -52,6 +64,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/4a/76/86/4a7686fd-5d4b-1ba1-9504-58a0908c2880/AppIcon-Main-0-0-1x_U007emarketing-0-7-0-sRGB-85-220.png/246x0w.webp",
     "name": "Waterllama - Nhắc nhở uống nước",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "309KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1zlyOdaGMU1uoy4i-rl7ASNL6bu2u-iS5"
   },
@@ -60,6 +74,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/69/75/ce/6975ce50-809a-2cac-eb02-70bce0ef1bbd/AppIcon-0-0-1x_U007emarketing-0-5-0-85-220.png/246x0w.webp",
     "name": "Cẩm nang cho Thợ điện",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "190KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/file/d/1RvL5e7L22A5l0MUzuDny03JKzJU4eFMu/view?usp=drive_link"
   },
@@ -68,6 +84,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/a5/e2/49/a5e24995-09c1-eb8f-4365-a9244b2199d2/AppIcon-0-1x_U007emarketing-0-10-0-sRGB-85-220.png/246x0w.webp",
     "name": "Beat Now: Youtube tắt màn hình",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "93KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1jO79fdhNNFKjUdU4DqCj_aRB74iP7Pet"
   },
@@ -76,6 +94,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/5b/2a/fa/5b2afaee-bd01-ac41-f510-5977977708c4/AppIcon-0-0-1x_U007epad-0-0-85-220.png/246x0w.webp",
     "name": "Musi",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "212KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1zJ3Y5-K5_KBH_X5vkAJtMB58hw0Ks-xy"
   },
@@ -84,6 +104,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/c2/37/a0/c237a031-37fb-1ccc-ba6d-973b88efae77/AppIcon-0-0-1x_U007emarketing-0-7-0-0-sRGB-85-220.png/246x0w.webp",
     "name": "My Diary - nhật ký có khóa",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "631KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=11nEHDef6S0pq-D6676W3oOFjpFh1i_C0"
   },
@@ -92,6 +114,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/b2/d6/c6/b2d6c66e-dcc2-8d70-2bb3-264433827f58/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-P3-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp",
     "name": "Sudoku",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "5,2MB",
     "description": "Premium vĩnh viễn.",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1E9pUNsYk0uNPVgsTmg8z_pCzpDYJuHTo"
   },
@@ -100,6 +124,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple125/v4/04/b4/4c/04b44c0f-8900-7129-3b4d-b9c2a66c2c38/AppIcon-1x_U007emarketing-0-6-85-220.png/246x0w.webp",
     "name": "The World ",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "N/A",
     "description": "Vô vạn tiền",
     "downloadLink": ""
   },
@@ -108,6 +134,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/c2/5f/15/c25f15e6-99ca-248c-41c0-af74e2e8fe83/AppIcon-0-0-1x_U007emarketing-0-7-0-sRGB-85-220.png/246x0w.webp",
     "name": "Video Saver PRO+",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "4,1MB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1cNLsPJ5unKuk1abXX6ZgB9ryUhnBMq6O"
   },
@@ -116,6 +144,8 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/74/0c/56/740c5666-42e7-6ff2-7fb6-3198c7c3ba2c/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp",
     "name": "Packr - Soạn hành lý cho chuyến đi",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "257KB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=1i4dQZ7dIxreTpGXoZzLKhayXEO77K2U1"
   },
@@ -124,7 +154,81 @@ const apps = [
     "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/06/f8/1f/06f81ff3-8f59-c02b-2ff3-45a67a49c609/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp",
     "name": "Love Game -  Trò chơi cho cặp đôi",
     "os": "iOS",
+    "time": "20/10/2023",
+    "size": "1,6MB",
     "description": "Premium vĩnh viễn",
     "downloadLink": "https://drive.google.com/uc?export=download&id=18ggLS2TblWRpfj6Esa8oEA96lipfwRNO"
-  }
+  },
+  {
+    "type": "Trick",
+    "logo": "./Data/IMG/LOGO/Microsoft-Toolkit.jpg",
+    "name": "Active Windows & Office",
+    "os": "Windows",
+    "time": "21/10/2023",
+    "size": "25,83MB",
+    "description": "Kích hoạt Win &  Office miễn phí.",
+    "downloadLink": "https://www.mediafire.com/file/g6ak25g09dv2loe/Microsoft_Toolkit_Full_Version.rar/file"
+  },
+  {
+    "type": "App",
+    "logo": "./Data/IMG/LOGO/OfficeToolPlus.jpg",
+    "name": "Office tool plus",
+    "os": "Windows",
+    "time": "21/10/2023",
+    "size": "10,35MB",
+    "description": "Tải và kích hoạt Office miễn phí.",
+    "downloadLink": "https://www.mediafire.com/file/ig95e3dzigypggp/Office_Tool_v10.4.2.4_x86.zip/file"
+  },
+  {
+    "type": "App",
+    "logo": "./Data/IMG/LOGO/WinRAR.jpg",
+    "name": "WinRAR & Key",
+    "os": "Windows",
+    "time": "21/10/2023",
+    "size": "3,24MB",
+    "description": "WinRAR v6.24 & Key kích hoạt. ",
+    "downloadLink": "https://www.mediafire.com/file/24ekhqs55d40xzg/WinRAR+Key.zip/file"
+  },
+  {
+    "type": "Trick",
+    "logo": "./Data/IMG/LOGO/dynamic-island.jpg",
+    "name": "Dynamic Island",
+    "os": "iOS",
+    "time": "21/10/2023",
+    "size": "Video",
+    "description": "Dynamic Island cho thiết bị cũ hơn. Lưu ý tuyệt đối chỉ sử dụng với iOS 16 trở lên.",
+    "downloadLink": "./Data/IMG/MORE/TrickiPhone1.mp4"
+  },
+  {
+    "type": "Trick",
+    "logo": "./Data/IMG/LOGO/Misaka.jpg",
+    "name": "Misaka 3.2.1",
+    "os": "iOS",
+    "time": "21/10/2023",
+    "size": "21.5Mb",
+    "description": "Quản lý Tweak. Gần giống với Jailbreak.",
+    "downloadLink": "./Data/IMG/MORE/misaka-v3.2.1.ipa"
+  },
+  {
+    "type": "App",
+    "logo": "./Data/IMG/LOGO/Sideloadly.png",
+    "name": "Sideloadly",
+    "os": "Windows",
+    "time": "21/10/2023",
+    "size": "126Mb",
+    "description": "Cài file IPA vào iPhone, iPad.",
+    "downloadLink": "https://www.mediafire.com/file/9ade41hgj23ngte/SideloadlySetup64.exe/file"
+  },
+  {
+    "type": "App",
+    "logo": "https://imazing.com/img/imazing-logo.svg",
+    "name": "iMazing",
+    "os": "Windows",
+    "time": "21/10/2023",
+    "size": "122Mb",
+    "description": "Chuyển dữ liệu các game, ứng dụng đã chỉnh sửa.",
+    "downloadLink": "https://imazing.com/"
+  },
+  
+  
 ];
--- Apps/Apps/Data/file.js
+++ Apps/Apps/Data/file.js
@@ -1,6 +1,5 @@
-window.onload = function()
-{
-    renderAppList();  
+window.onload = function () {
+  renderAppList();
 };
 
 const searchInputElement = document.getElementById("search-input");
@@ -16,59 +15,95 @@ typeSelectElement.addEventListener("change", renderAppList);
 
 
 
-  function renderAppList() {
-    appListElement.innerHTML = "";
-    
-    const searchTerm = searchInputElement.value.toLowerCase();
-    const osFilter = osSelectElement.value;
-    const typeFilter = typeSelectElement.value || "";
-  
-    const filteredApps = apps.filter((app) => {
-      return (
-        app.name.toLowerCase().includes(searchTerm) &&
-        (osFilter === "" || app.os === osFilter) &&
-        (typeFilter === "" || app.type === typeFilter)
-      );
+function renderAppList() {
+  appListElement.innerHTML = "";
+
+  const searchTerm = searchInputElement.value.toLowerCase();
+  const osFilter = osSelectElement.value;
+  const typeFilter = typeSelectElement.value || "";
+
+  const filteredApps = apps.filter((app) => {
+    return (
+      app.name.toLowerCase().includes(searchTerm) &&
+      (osFilter === "" || app.os === osFilter) &&
+      (typeFilter === "" || app.type === typeFilter)
+    );
+  });
+
+  if (filteredApps.length === 0) {
+    const noDataElement = document.createElement("div");
+    noDataElement.classList.add("no-data");
+    noDataElement.innerText = "Không có dữ liệu";
+    appListElement.appendChild(noDataElement);
+  } else {
+    filteredApps.forEach((app) => {
+      const appElement = document.createElement("div");
+      appElement.classList.add("App");
+      let osIcon = '';
+      if (app.os === "Android") {
+        osIcon = '<i class="fab fa-android"></i> Android';
+      } else if (app.os === "iOS") {
+        osIcon = '<i class="fab fa-apple"></i> iOS';
+      } else if (app.os === "Windows") {
+        osIcon = '<i class="fab fa-windows"></i> Windows';
+      } else {
+        osIcon = '<i class="fa fa-question-circle"></i> Khác';
+      }
+
+      if (app.type === "Game") {
+        type = '<i class="fa-solid fa-gamepad"></i> Trò chơi';
+      } else if (app.type === "App") {
+        type = '<i class="fa-solid fa-cube"></i> Ứng dụng';
+      } else if (app.type === "Trick") {
+        type = '<i class="fa-solid fa-wand-magic-sparkles"></i> Thủ thuật';
+      } else {
+        type = '<i class="fa-solid fa-question"></i>';
+      }
+
+      
+      if (app.time == undefined) {
+        time = '<i class="fa-solid fa-question"></i>';
+      }else {
+        time = '<i class="fa-regular fa-calendar"></i> ' + app.time 
+      }
+
+      if (app.size == undefined) {
+        size = 'N/A';
+      }else {
+        size = app.size
+      }
+
+      
+      appElement.innerHTML = `
+      <div class="InfoApp">
+        <div class="Info">
+          <div class="Icon">
+            <img src="${app.logo}" alt="${app.name}">
+          </div>
+          <div class="Name">
+            <h1>${app.name}</h1>
+            <p>${app.description}</p>
+          </div>
+      </div>
+      <div class="Download">
+        <a href="${app.downloadLink}">
+          <button>
+            <i class="fa-solid fa-download"></i>
+          </button>
+        </a>
+      </div>
+    </div>
+    <div class="More">
+      <div class="OStype">${osIcon}</div>
+      <div class="Type">${type}</div>
+      <div class="Category">${time}</div>
+      <div class="Storage"><i class="fa-solid fa-database"></i> ${size}</div>
+      
+    </div>
+`;
+      // ${app.downloadLink}
+      appListElement.appendChild(appElement);
     });
-  
-    if (filteredApps.length === 0) {
-      const noDataElement = document.createElement("div");
-      noDataElement.classList.add("no-data");
-      noDataElement.innerText = "Không có dữ liệu";
-      appListElement.appendChild(noDataElement);
-    } else {
-        filteredApps.forEach((app) => {
-            const appElement = document.createElement("div");
-            appElement.classList.add("app");
-            let osIcon = '';
-            if (app.os === "Android") {
-              osIcon = '<i class="fab fa-android"></i>';
-            } else if (app.os === "iOS") {
-              osIcon = '<i class="fab fa-apple"></i>';
-            } else if (app.os === "Windows") {
-              osIcon = '<i class="fab fa-windows"></i>';
-            } else {
-              osIcon = '<i class="fa fa-question-circle"></i>';
-            }
-            appElement.innerHTML = `
-              <img src="${app.logo}" alt="${app.name} logo">
-              <div class="info">
-                <div class="infoApp">
-                    <h2>${app.name}</h2>
-                    <p>${osIcon}</p>
-                </div>
-                <p>${app.description}</p>
-              </div>
-              <div class="download">
-                <a href="${app.downloadLink}">
-                  <button>Tải về</button>
-                </a>
-              </div>
-            `;
-            // ${app.downloadLink}
-            appListElement.appendChild(appElement);
-          });
-    }
   }
-  
-  
+}
+
