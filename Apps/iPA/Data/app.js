38fb88c168cb7fe0c23734d47fa116267456dc3d
Chesino <45505217+chesino@users.noreply.github.com>
2023-12-01 14:57:48 +0700

    HunqD

--- Apps/iPA/Data/app.js
+++ Apps/iPA/Data/app.js
@@ -209,5 +209,45 @@ const apps = [
     "description": "Chặn quảng cáo, Nghe nhạc màn hình, PiP,...",
     "downloadLink": "https://download1076.mediafire.com/9r6dtdblj3hgaDYV0wjCOpkSWufjmwsaZuP8-FTa4flR-YkgXu0Cau2GkR0OXFOR56JlHua1BWyQt4eg_qs8w_oZNDA_JbDTfF1YphX_wo6fjMjZgwQOOty-lhy5aYRwbQ_El57I4j8Zh33IY1vvqMuSxHUjAYMWUyHJ08qNDRR_Gmc/zjjs25xodrmokjb/DiuTube_18.41.2_LeTi.ipa"
   },
+  {
+    "type": "iPA",
+    "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/d9/ef/57/d9ef57c0-7e6d-627d-2a95-0b69ece08466/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/230x0w.webp",
+    "name": "RenpyViewer",
+    "os": "iOS",
+    "version": "2.1.0",
+    "size": "101,6MB",
+    "description": "Chơi Game Ren'py trên iPhone.",
+    "downloadLink": "https://download1324.mediafire.com/xzko7slaeingL8o0AVhjoMcRnoNieut7Ay1MkFWZSWlVTw3Rt2g21mFpJ2BF3rZMf5hqVjWfIlfpMv_W2BuSG2tmvK1GA71UPGcQFWAWC3lvfkm2SCLnHdh3qjeZMucWO50TS91H5oUnboHaEW9ZdsFtzr4vuE2s0v2T_NJydiVvKfU/ibllwkjrs6rc9k7/RenpyViewer+3.5%5BiOS+13%5D.ipa"
+  },
+  {
+    "type": "iPA",
+    "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/2f/db/b5/2fdbb59a-f65c-d40e-91a6-00beb25daf45/AppIcon-0-0-1x_U007emarketing-0-10-0-0-sRGB-85-220.png/230x0w.webp",
+    "name": "ONSPlayer",
+    "os": "iOS",
+    "version": "3.7",
+    "size": "13,9MB",
+    "description": "Chơi Game RPGMV trên iPhone ( Phải có chứa File WWW ).",
+    "downloadLink": "https://download1593.mediafire.com/e1exwmfjwdugRdQ34oAkDUPDtmx28BxjhIzK0few5071MvZCeSp9bIfkkLConY-MMx5aYOanOVaUrazFgHvLMtoEABUEVtXfkdCTaT46uw1GoZsWkKI4qSBPlFpmV_evroXzcnjmXNUrw52SEGq9GS-mB5Ud8lGLjyohtPSXFShSXdA/ubbw6muf0iwl570/ONSPlayer+3.7.ipa"
+  },
+  {
+    "type": "iPA",
+    "logo": "https://is1-ssl.mzstatic.com/image/thumb/Purple115/v4/f7/65/d3/f765d38c-c890-fcb2-26c4-1662e998f709/AppIcon-1x_U007emarketing-0-6-85-220.png/230x0w.webp",
+    "name": "XP3Player",
+    "os": "iOS",
+    "version": "1.3.11",
+    "size": "15,7MB",
+    "description": "Chơi Game Visual novel trên iPhone.",
+    "downloadLink": "https://download1591.mediafire.com/1djhb1slprjgDXQkHPA8x6e79yh4Bu-uK1HulgVGo7zFLfQLnFkTiWkC1hDCRJmNULSoLjNcF25wD7wnkjSwlg9_lh_ts7b62h1_GjIAOyMVQexEAkXP6yjDX3cSaWNraPRp83GiX96wwE9Fqiln-gAmexhiXLSXlUcKQ6br8aax/3fn8vg75njvvws8/XP3Player+1.3.11.ipa"
+  },
+  {
+    "type": "Tweak",
+    "logo": "./Data/LogoApps/auto-clicker.png",
+    "name": "Auto Clicker ",
+    "os": "iOS",
+    "version": "1.0",
+    "size": "9.52MB",
+    "description": "Tự động nhấn, thao tác cảm ứng.",
+    "downloadLink": "https://download1326.mediafire.com/yqbr57ue9vcgaqnXaiCrDN2nyyOJMRMg-N1FavYF6gjAPBJAv0m9PSVbOT9WrEHQSpbxaJAyksIcXNtRudjfJmni1VJWOYsLVs-GRoe73_fMgo1cbyjH5ZQt101yhbpbCXa__zFIbkvOPGGyyg76x9sxp7n_Kx2MzF9hjuan6-79ikg/42eb9ocs8ppp75g/Auto-Clicker.ipa"
+  },
 ];
 
