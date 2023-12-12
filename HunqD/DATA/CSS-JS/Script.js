b49b07338d9e3f49e00bad78e43d82dc96c7785a
Chesino <45505217+chesino@users.noreply.github.com>
2023-12-12 16:34:37 +0700

    HunqD

---
+++ HunqD/DATA/CSS-JS/Script.js
@@ -0,0 +1,356 @@
+const jsonData = [
+  [
+    {
+      "name": "Hùng Đinh",
+      "profileURL": "https://www.facebook.com/profile.php?id=61551995024526",
+      "timestamp": 1697068800,
+      "id": 61551995024526,
+      "pass": "111"
+
+    },
+    {
+      "name": "Ánh Hằng",
+      "profileURL": "https://www.facebook.com/profile.php?id=100092307943151",
+      "timestamp": 1701959002,
+      "id": 100092307943151,
+      "pass": "123"
+    },
+    {
+      "name": "Trần Ngọc Ánh",
+      "profileURL": "https://www.facebook.com/profile.php?id=100088260422422",
+      "timestamp": 1697116606,
+      "id": 100088260422422,
+      "pass": "123"
+    },
+    {
+      "name": "Hải Yến",
+      "profileURL": "https://www.facebook.com/profile.php?id=100073434319397",
+      "timestamp": 1701998563,
+      "id": 100073434319397,
+      "pass": "123"
+    },
+    {
+      "name": "Phuong Linh",
+      "profileURL": "https://www.facebook.com/profile.php?id=100063894533991",
+      "timestamp": 1702186683,
+      "id": 100063894533991,
+      "pass": "123"
+    },
+    {
+      "name": "Ngọc Linh",
+      "profileURL": "https://www.facebook.com/profile.php?id=100060666301063",
+      "timestamp": 1697275904,
+      "id": 100060666301063,
+      "pass": "123"
+    },
+    {
+      "name": "Nisi Kawa",
+      "profileURL": "https://www.facebook.com/profile.php?id=100057501075921",
+      "timestamp": 1702021105,
+      "id": 100057501075921,
+      "pass": "123"
+    },
+    {
+      "name": "Trọng Nghĩa",
+      "profileURL": "https://www.facebook.com/profile.php?id=100052261622413",
+      "timestamp": 1702295638,
+      "id": 100052261622413,
+      "pass": "123"
+    },
+    {
+      "name": "Nhi Rex",
+      "profileURL": "Nhi Rex",
+      "pass": "123"
+    },
+    {
+      "name": "Nhỏ Nguyễn",
+      "profileURL": "https://www.facebook.com/profile.php?id=100044510499977",
+      "timestamp": 1697286855,
+      "id": 100044510499977,
+      "pass": "123"
+    },
+    {
+      "name": "Phan Mai Ly",
+      "profileURL": "https://www.facebook.com/profile.php?id=100042744378107",
+      "timestamp": 1701868351,
+      "id": 100042744378107,
+      "pass": "123"
+    },
+    {
+      "name": "Tiến Đạt",
+      "profileURL": "https://www.facebook.com/profile.php?id=100042140778354",
+      "timestamp": 1697278179,
+      "id": 100042140778354,
+      "pass": "123"
+    },
+    {
+      "name": "Vo Hoang Phi Nhung",
+      "profileURL": "https://www.facebook.com/ins.pnkunn",
+      "timestamp": 1702269050,
+      "id": 100035657791086,
+      "pass": "123"
+    },
+    {
+      "name": "Ngô Khanh",
+      "profileURL": "https://www.facebook.com/ngo.h.khanh.716",
+      "timestamp": 1697112574,
+      "id": 100034337931817,
+      "pass": "123"
+    },
+    {
+      "name": "Quang Thiện",
+      "profileURL": "https://www.facebook.com/q.thien.vu.9",
+      "timestamp": 1697114063,
+      "id": 100028302531768,
+      "pass": "123"
+    },
+    {
+      "name": "Bùi Thanh Hiền",
+      "profileURL": "https://www.facebook.com/hien40555",
+      "timestamp": 1697111769,
+      "id": 100022625414871,
+      "pass": "123"
+    },
+    {
+      "name": "Kiều My",
+      "profileURL": "https://www.facebook.com/profile.php?id=100022277839522",
+      "timestamp": 1702221540,
+      "id": 100022277839522,
+      "pass": "123"
+    },
+    {
+      "name": "Lê Ngọc Trầm",
+      "profileURL": "https://www.facebook.com/tram.lengoctram.948",
+      "timestamp": 1701954695,
+      "id": 100015979231019,
+      "pass": "123"
+    },
+    {
+      "name": "Nhật Hạ",
+      "profileURL": "https://www.facebook.com/nhatha941",
+      "timestamp": 1697276525,
+      "id": 100014817736634,
+      "pass": "123"
+    },
+    {
+      "name": "Tiến Lê",
+      "profileURL": "https://www.facebook.com/profile.php?id=100014569175965",
+      "timestamp": 1697276841,
+      "id": 100014569175965,
+      "pass": "123"
+    },
+    {
+      "name": "Minh Quân",
+      "profileURL": "https://www.facebook.com/quan31702",
+      "timestamp": 1697275928,
+      "id": 100014270777334,
+      "pass": "123"
+    },
+    {
+      "name": "Thanh Anh",
+      "profileURL": "https://www.facebook.com/thanhanh66",
+      "timestamp": 1697282191,
+      "id": 100012949321472,
+      "pass": "123"
+    },
+    {
+      "name": "Ân Thanh Tân",
+      "profileURL": "https://www.facebook.com/tandeptrai29122002",
+      "timestamp": 1697283245,
+      "id": 100010685404349,
+      "pass": "123"
+    },
+    {
+      "name": "Nguyễn AnhDuy",
+      "profileURL": "https://www.facebook.com/nguyenahdyy",
+      "timestamp": 1697276964,
+      "id": 100008161964802,
+      "pass": "123"
+    },
+    {
+      "name": "Trung Nguyễn",
+      "profileURL": "https://www.facebook.com/trung22121998",
+      "timestamp": 1697385319,
+      "id": 100008045827167,
+      "pass": "123"
+    },
+    {
+      "name": "Nâu",
+      "profileURL": "https://www.facebook.com/naucuabame",
+      "timestamp": 1697111612,
+      "id": 100004709207159,
+      "pass": "123"
+    },
+    {
+      "name": "Hao Tu",
+      "profileURL": "https://www.facebook.com/profile.php?id=61552165441962",
+      "timestamp": 1701964236,
+      "id": 61552165441962,
+      "pass": "123"
+    },
+    {
+      "name": "Lê Đức Thảnh",
+      "profileURL": "https://www.facebook.com/profile.php?id=61551121340761",
+      "timestamp": 1697280584,
+      "id": 61551121340761,
+      "pass": "123"
+    },
+    {
+      "name": "Hồ Ngọc Tâm Nhi",
+      "profileURL": "https://www.facebook.com/profile.php?id=61551075231497",
+      "timestamp": 1702271524,
+      "id": 61551075231497,
+      "pass": "123"
+    },
+    {
+      "name": "Thụy Ngân",
+      "profileURL": "https://www.facebook.com/ngandz100",
+      "timestamp": 1702350499,
+      "id": 100017818543172,
+      "pass": "123"
+    },
+
+  ]
+];
+
+function SignIn() {
+  const searchInput = document.getElementById('searchInput').value;
+  const passwordInput = document.getElementById('passwordInput').value; // Thêm dòng này để lấy mật khẩu
+  const resultDiv = document.getElementById('result');
+
+  const result = searchByNameOrID(searchInput);
+
+  if (result && result.pass === passwordInput) { // Kiểm tra mật khẩu
+    const formattedTime = formatTimestamp(result.timestamp);
+    const formattedTime2 = formatTimestamp2(result.timestamp);
+    if (result.id === undefined) {
+      result.id = 'Chưa cập nhật ID'
+      img = '/DATA/logo.png';
+
+    } else {
+      result.id
+      img = `https://graph.facebook.com/${result.id}/picture?width=9999&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`;
+    }
+    resultDiv.innerHTML = `
+
+        <div class="Head">
+            <div class="Avatar">
+                <img src="${img}" alt="" srcset="">
+            </div>
+            <div class="Icon">
+                <i class="fa-solid fa-link"></i>
+            </div>
+            <div class="Avatar">
+                <img src="https://graph.facebook.com/61551995024526/picture?width=9999&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662" alt="" srcset="">
+            </div>
+        </div>
+        <div class="Time">
+            <h2> ${result.name}, bạn đã kết nối với Hùng</h2>
+            <h1>${formattedTime2}</h1>
+        </div>
+        <div class="More">
+            <div class="Card">
+                <div class="Label">
+                    Thời gian kết bạn
+                </div>
+                <div class="Output">
+                    ${formattedTime}
+                </div>
+            </div>
+            <div class="Card">
+                <div class="Label">
+                    ID của bạn
+                </div>
+                <div class="Output">
+                    ${result.id}
+                </div>
+            </div>
+        </div>
+      `;
+  } else {
+    resultDiv.innerHTML = '<p>Bạn không có trong danh sách bạn bè hoặc nhập sai dữ liệu.</p>';
+  }
+}
+
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp * 1000); // Đổi timestamp thành mili giây
+  return date.toLocaleString(); // Sử dụng phương thức toLocaleString() để định dạng ngày giờ
+}
+
+function formatTimestamp2(timestamp) {
+  const now = new Date();
+  const date = new Date(timestamp * 1000);
+
+  const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+
+  const nowFormatted = now.toLocaleDateString('en-US', options);
+  const dateFormatted = date.toLocaleDateString('en-US', options);
+
+  console.log(`Ngày hiện tại: ${nowFormatted}`);
+  console.log(`Ngày từ timestamp: ${dateFormatted}`);
+
+  // Tính số ngày chênh lệch
+  const timeDiff = now.getTime() - date.getTime();
+  const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+  const yearsDiff = Math.floor(daysDiff / 365);
+  const monthsDiff = Math.floor((daysDiff % 365) / 30);
+  const remainingDays = daysDiff % 30;
+
+  let result = '';
+
+  if (yearsDiff > 0) {
+    result = `${remainingDays} ngày ${monthsDiff} tháng ${yearsDiff} năm`;
+  } else if (monthsDiff > 0) {
+    result = `${remainingDays} ngày ${monthsDiff} tháng`;
+  } else {
+    result = `${remainingDays} ngày`;
+  }
+
+  return result;
+}
+
+
+function extractFacebookProfileURL(input) {
+  // Sử dụng regular expression để trích xuất URL
+  const match = input.match(/(https:\/\/www\.facebook\.com\/profile\.php\?id=\d+)/);
+
+  // Nếu có kết quả, trả về URL, ngược lại trả về null
+  return match ? match[1] : null;
+}
+
+function searchByNameOrID(query) {
+  // Kiểm tra nếu query chứa URL đầy đủ
+  const facebookURL = extractFacebookProfileURL(query);
+  if (facebookURL) {
+    // Nếu có URL, thực hiện tìm kiếm bằng URL
+    for (const data of jsonData[0]) {
+      if (data.profileURL === facebookURL) {
+        return data;
+      }
+    }
+  } else {
+    // Nếu không phải URL đầy đủ, tiếp tục tìm kiếm bằng tên hoặc ID
+    for (const data of jsonData[0]) {
+      if (data.name === query || data.id === query || data.profileURL === query) {
+        return data;
+      }
+    }
+  }
+
+  // Nếu không tìm thấy, trả về null
+  return null;
+}
+function SignUp() {
+  const searchInput = document.getElementById('searchInput').value;
+  const passwordInput = document.getElementById('passwordInput').value; // Thêm dòng này để lấy mật khẩu
+  const resultText = document.getElementById('resultText');
+
+  if (searchInput !== '' && passwordInput !== '') {
+    const redirectUrl = `http://m.me/ChesinoPage?text=${searchInput}:${passwordInput}`;
+    window.open(redirectUrl, '_blank');
+  } else {
+    resultText.innerText = 'Vui lòng điền thông tin để đăng ký'
+  }
+
+}
