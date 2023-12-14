a5e62191e5a1d8628004faca15a538657c0afc93
Chesino <45505217+chesino@users.noreply.github.com>
2023-12-15 01:23:44 +0700

    HunqD

---
+++ HunqD/DATA/Friends.js
@@ -0,0 +1,330 @@
+const jsonData = [
+    [
+      {
+        "name": "Hùng Đinh",
+        "profileURL": "https://www.facebook.com/profile.php?id=61551995024526",
+        "timestamp": 1697068800,
+        "id": 61551995024526,
+        "point": 22,
+        "pass": "111"
+  
+      },
+      {
+        "name": "Ánh Hằng",
+        "profileURL": "https://www.facebook.com/profile.php?id=100092307943151",
+        "timestamp": 1701959002,
+        "id": 100092307943151,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Trần Ngọc Ánh",
+        "profileURL": "https://www.facebook.com/profile.php?id=100088260422422",
+        "timestamp": 1697116606,
+        "id": 100088260422422,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Hải Yến",
+        "profileURL": "https://www.facebook.com/profile.php?id=100073434319397",
+        "timestamp": 1701998563,
+        "id": 100073434319397,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Phuong Linh",
+        "profileURL": "https://www.facebook.com/profile.php?id=100063894533991",
+        "timestamp": 1702186683,
+        "id": 100063894533991,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Ngọc Linh",
+        "profileURL": "https://www.facebook.com/profile.php?id=100060666301063",
+        "timestamp": 1697275904,
+        "id": 100060666301063,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Nisi Kawa",
+        "profileURL": "https://www.facebook.com/profile.php?id=100057501075921",
+        "timestamp": 1702021105,
+        "id": 100057501075921,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Trọng Nghĩa",
+        "profileURL": "https://www.facebook.com/profile.php?id=100052261622413",
+        "timestamp": 1702295638,
+        "id": 100052261622413,
+        "point": 0,
+        "pass": "123"
+      },
+      // {
+      //   "name": "Nhi Rex",
+      //   "profileURL": "Nhi Rex",
+      //   "rank": "Bạn FB",
+      // "point": 0,
+      // "pass": "123"
+      // },
+      {
+        "name": "Nhỏ Nguyễn",
+        "profileURL": "https://www.facebook.com/profile.php?id=100044510499977",
+        "timestamp": 1697286855,
+        "id": 100044510499977,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Phan Mai Ly",
+        "profileURL": "https://www.facebook.com/profile.php?id=100042744378107",
+        "timestamp": 1701868351,
+        "id": 100042744378107,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Tiến Đạt",
+        "profileURL": "https://www.facebook.com/profile.php?id=100042140778354",
+        "timestamp": 1697278179,
+        "id": 100042140778354,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Vo Hoang Phi Nhung",
+        "profileURL": "https://www.facebook.com/ins.pnkunn",
+        "timestamp": 1702269050,
+        "id": 100035657791086,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Ngô Khanh",
+        "profileURL": "https://www.facebook.com/ngo.h.khanh.716",
+        "timestamp": 1697112574,
+        "id": 100034337931817,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Quang Thiện",
+        "profileURL": "https://www.facebook.com/q.thien.vu.9",
+        "timestamp": 1697114063,
+        "id": 100028302531768,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Bùi Thanh Hiền",
+        "profileURL": "https://www.facebook.com/hien40555",
+        "timestamp": 1697111769,
+        "id": 100022625414871,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Kiều My",
+        "profileURL": "https://www.facebook.com/profile.php?id=100022277839522",
+        "timestamp": 1702221540,
+        "id": 100022277839522,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Lê Ngọc Trầm",
+        "profileURL": "https://www.facebook.com/tram.lengoctram.948",
+        "timestamp": 1701954695,
+        "id": 100015979231019,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Nhật Hạ",
+        "profileURL": "https://www.facebook.com/nhatha941",
+        "timestamp": 1697276525,
+        "id": 100014817736634,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Tiến Lê",
+        "profileURL": "https://www.facebook.com/profile.php?id=100014569175965",
+        "timestamp": 1697276841,
+        "id": 100014569175965,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Minh Quân",
+        "profileURL": "https://www.facebook.com/quan31702",
+        "timestamp": 1697275928,
+        "id": 100014270777334,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Thanh Anh",
+        "profileURL": "https://www.facebook.com/thanhanh66",
+        "timestamp": 1697282191,
+        "id": 100012949321472,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Ân Thanh Tân",
+        "profileURL": "https://www.facebook.com/tandeptrai29122002",
+        "timestamp": 1697283245,
+        "id": 100010685404349,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Nguyễn AnhDuy",
+        "profileURL": "https://www.facebook.com/nguyenahdyy",
+        "timestamp": 1697276964,
+        "id": 100008161964802,
+        "point": 0,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Trung Nguyễn",
+        "profileURL": "https://www.facebook.com/trung22121998",
+        "timestamp": 1697385319,
+        "id": 100008045827167,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Nâu",
+        "profileURL": "https://www.facebook.com/naucuabame",
+        "timestamp": 1697111612,
+        "id": 100004709207159,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Hao Tu",
+        "profileURL": "https://www.facebook.com/profile.php?id=61552165441962",
+        "timestamp": 1701964236,
+        "id": 61552165441962,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Lê Đức Thảnh",
+        "profileURL": "https://www.facebook.com/profile.php?id=61551121340761",
+        "timestamp": 1697280584,
+        "id": 61551121340761,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Hồ Ngọc Tâm Nhi",
+        "profileURL": "https://www.facebook.com/profile.php?id=61551075231497",
+        "timestamp": 1702271524,
+        "id": 61551075231497,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Thụy Ngân",
+        "profileURL": "https://www.facebook.com/ngandz100",
+        "timestamp": 1702350499,
+        "id": 100017818543172,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Quynh Anh",
+        "profileURL": "https://www.facebook.com/profile.php?id=100070807361760",
+        "timestamp": 1702550001,
+        "id": 100070807361760,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Phạm Thị Quỳnh Trang",
+        "profileURL": "https://www.facebook.com/profile.php?id=100055032565147",
+        "timestamp": 1702549840,
+        "id": 100055032565147,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Nguyễn Thảo Vy",
+        "profileURL": "https://www.facebook.com/profile.php?id=100033502783842",
+        "timestamp": 1702549125,
+        "id": 100033502783842,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Ng Hoang Maii Thyy",
+        "profileURL": "https://www.facebook.com/tonii.bede",
+        "timestamp": 1702521702,
+        "id": 100060435398567,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Thuyy Trangg",
+        "profileURL": "https://www.facebook.com/profile.php?id=100051307060695",
+        "timestamp": 1702472108,
+        "id": 100051307060695,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "An An",
+        "profileURL": "https://www.facebook.com/lethucan.huynh",
+        "timestamp": 1702463806,
+        "id": 100056525235726,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Ngọc Trân",
+        "profileURL": "https://www.facebook.com/profile.php?id=100034553970517",
+        "timestamp": 1702462537,
+        "id": 100034553970517,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Ngọc Diệu",
+        "profileURL": "https://www.facebook.com/profile.php?id=100060300762174",
+        "timestamp": 1702461652,
+        "id": 100060300762174,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Nhã Mai",
+        "profileURL": "https://www.facebook.com/duong.thuylinh.102",
+        "timestamp": 1702459900,
+        "id": 100014439854852,
+        "point": 0,
+        "pass": "123"
+      },
+      {
+        "name": "Thụy Ngân",
+        "profileURL": "https://www.facebook.com/ngandz100",
+        "timestamp": 1702350499,
+        "id": 100017818543172,
+        "point": 0,
+        "pass": "123"
+      },
+  
+    ]
+  ];
+
+
+
+ 
