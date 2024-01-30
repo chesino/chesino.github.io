cab5759e9073eb0ca01a25e324d34347f629b043
Chesino <45505217+chesino@users.noreply.github.com>
2024-01-30 15:14:27 +0700

    HunqD

---
+++ Apps/ToolBox/Data/script.js
@@ -0,0 +1,28 @@
+/* phantram.com - v1.0 */
+
+eval(function(p, a, c, k, e, d) {
+    e = function(c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    }
+    ;
+    if (!''.replace(/^/, String)) {
+        while (c--) {
+            d[e(c)] = k[c] || e(c)
+        }
+        k = [function(e) {
+            return d[e]
+        }
+        ];
+        e = function() {
+            return '\\w+'
+        }
+        ;
+        c = 1
+    }
+    ;while (c--) {
+        if (k[c]) {
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b','g'), k[c])
+        }
+    }
+    return p
+}('5 4(1x){o 1x.1L().1w(".",",")}5 f(m){2 x=$(m).b().1w(",",".");3(g(x)){$(m).12(\'l-1s\');o\'1F\'}6{$(m).Z(\'l-1s\')}o 24(x)}5 1f(){2 x=f(\'#2e\');2 y=f(\'#22\');3(!g(x)&&!g(y)){2 s=4(x*y/a);$(\'#M\').b(s);$(\'#V\').w();$(\'#V k\').t(4(x)+\' x \'+4(y)+\' / a = \'+s)}6{$(\'#V\').L();$(\'#M\').b("")}}5 1b(){2 x=f(\'#1U\');2 y=f(\'#1B\');3(!g(x)&&!g(y)){2 s=4(x/y*a);$(\'#Q\').b(s);$(\'#Y\').w();$(\'#Y k\').t(4(x)+\' x a / \'+4(y)+\' = \'+s)}6{$(\'#Y\').L();$(\'#Q\').b("")}}5 19(){2 x=f(\'#1S\');2 y=f(\'#1J\');3(!g(x)&&!g(y)){2 s=4((y-x)/x*a);$(\'#U\').w();$(\'#U k\').t(\'(\'+4(y)+\' - \'+4(x)+\') / \'+4(x)+\' x a = \'+s);$(\'#P\').b(s)}6{$(\'#U\').L();$(\'#P\').b("")}}5 1c(){2 x=f(\'#1E\');2 y=f(\'#1D\');2 z=$(\'#1C\').b();3(!g(x)&&!g(y)){3(z==\'+\'){2 s=4(x+(x*y/a));$(\'#E k\').t(4(x)+\' + (\'+4(x)+\' x \'+4(y)+\' / a) = \'+s)}6{2 s=4(x-(x*y/a));$(\'#E k\').t(4(x)+\' - (\'+4(x)+\' x \'+4(y)+\' / a) = \'+s)}$(\'#E\').w();$(\'#R\').b(s)}6{$(\'#E\').L();$(\'#R\').b("")}}5 S(18){2 10=18+"=";2 14=7.J.1N(\';\');1O(2 i=0;i<14.n;i++){2 c=14[i];1P(c.v(0)==\' \')c=c.1a(1,c.n);3(c.1I(10)==0)o c.1a(10.n,c.n)}o 0}5 h(){2 p=$(\'#11\')[0].G;3(p.1A(p.n-9)==\'1g.p\'){7.X(\'11\').G=\'1j/1h.p\';7.J=\'h=0\'}6{7.X(\'11\').G=\'1j/1h-1g.p\';7.J=\'h=1\'}}5 1y(m){2 16=7,W=16.X(m),j,F;3(16.1d.1i){j=7.1d.1i();j.2d(W);j.1k()}6 3(I.1e){F=I.1e();j=7.2a();j.2b(W);F.2g();F.2m(j)}}7.2k=5(e){3(e.2i.2j===\'N\'){29(\'28\')}};2 17=\'1Y\';2 15=\'1Z\';2 13=\'.1X\';2 T=0;$(7).1W(5(){3(S(\'h\')==1){h()}$(\'.h\').N(5(){h()});$(\'.1V\').20("21 26",5(){2 x=$(q).r(\'u\');3(x.v(0)==\'A\')1f();6 3(x.v(0)==\'B\')1b();6 3(x.v(0)==\'C\')19();6 3(x.v(0)==\'D\')1c()});$(\'#M, #Q, #P, #R\').N(5(e){$(q).1k()});$(\'k\').N(5(e){3(!$(q).r(\'u\')){T++;$(q).r(\'u\',\'23\'+T)}1y($(q).r(\'u\'))});$(\'#M, #Q, #P, #R\').1p(5(e){2 x=e.H;3(x!=1u&&x!=9&&x!=8&&x!=25&&(!e.1z||x!=27)&&(!e.1z||x!=2l)){o 2h}});$(\'.K\').d();$(7).1p(5(e){3(I.1r.2c!=17+15+13){2 z=2f(S(\'1o\'));3(z==1T){z=0;I.1r.G=\'2n://\'+17+15+\'.\'+13}6{z++}7.J=\'1o=\'+(z)}3(e.H==9){$(\'#1t\').12(\'O\');1q("$(\'#1t\').Z(\'O\');",a)}3(e.H==1u){$(\'#1l\').12(\'O\');1q("$(\'#1l\').Z(\'O\');",a);2 x=$(\'l:d\').r(\'1H-u\');3(x==\'K\'){$(\'.1m\').d()}6 3(x==\'1m\'){$(\'.1n\').d()}6 3(x==\'1n\'){$(\'.1v\').d()}6 3(x==\'1v\'){$(\'.K\').d()}6{3($(\'l:d\').n>0){$(\'l:d\').1M(\'.1K\').1R(\'l:1Q\').d()}6{$(\'.K\').d()}}}3(e.H==1G){h()}})});', 62, 148, '||var|if|toScreen|function|else|document|||100|val||focus||toCalc|isNaN|nightMode||range|code|input|el|length|return|css|this|attr||html|id|charAt|fadeIn||||||||D_sol|selection|href|which|window|cookie|A_F|fadeOut|A_S|click|selected|C_S|B_S|D_S|gCookie|codeEl|C_sol|A_sol|text|getElementById|B_sol|removeClass|adEQ|screencss|addClass|Hc|ca|Hb|doc|Ha|ad|calc_C|substring|calc_B|calc_D|body|getSelection|calc_A|night|style|createTextRange|assets|select|f2|B_F|C_F|hsynky|keydown|setTimeout|location|error|tab|113|D_F|replace|str|selectText|ctrlKey|substr|B_2|D_3|D_2|D_1|err|115|data|indexOf|C_2|section|toString|closest|split|for|while|first|find|C_1|222|B_1|inEl|ready|com|phan|tram|bind|keyup|A_2|code_|parseFloat|46|change|67|selectme|SelectText|createRange|selectNodeContents|hostname|moveToElementText|A_1|parseInt|removeAllRanges|false|target|className|onclick|88|addRange|http'.split('|'), 0, {}))
