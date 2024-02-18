22b158b7891ec7db0c92c2ccd97440c25ddc131c
Chesino <45505217+chesino@users.noreply.github.com>
2024-02-18 23:51:54 +0700

    HunqD

--- HunqD/DATA/Tools/FriendsList/script.js
+++ HunqD/DATA/Tools/FriendsList/script.js
@@ -166,6 +166,146 @@ var fileOriginal = [
         }
     ]
 ][0];
+var fileOriginal2 = [
+    [
+        {
+            "name": "Trang cá nhân của bạn",
+            "profileURL": ""
+        },
+        {
+            "name": "Nhỏ Nguyễn",
+            "profileURL": "https://www.facebook.com/profile.php?id=100044510499977"
+        },
+        {
+            "name": "Ánh Hằng",
+            "profileURL": "https://www.facebook.com/profile.php?id=100092307943151"
+        },
+        {
+            "name": "Trần Ngọc Ánh",
+            "profileURL": "https://www.facebook.com/profile.php?id=100088260422422"
+        },
+        {
+            "name": "Hasumiki Yuna ",
+            "profileURL": "https://www.facebook.com/YunaLoliOfficial"
+        },
+        {
+            "name": "Hải Yến",
+            "profileURL": "https://www.facebook.com/profile.php?id=100073434319397"
+        },
+        {
+            "name": "Ngọc Linh",
+            "profileURL": "https://www.facebook.com/profile.php?id=100060666301063"
+        },
+        {
+            "name": "Ng Hoang Maii Thyy",
+            "profileURL": "https://www.facebook.com/tonii.bede"
+        },
+        {
+            "name": "Ngọc Diệu",
+            "profileURL": "https://www.facebook.com/profile.php?id=100060300762174"
+        },
+        {
+            "name": "An An",
+            "profileURL": "https://www.facebook.com/lethucan.huynh"
+        },
+        {
+            "name": "Trọng Nghĩa",
+            "profileURL": "https://www.facebook.com/profile.php?id=100052261622413"
+        },
+        {
+            "name": "Thuyy Trangg",
+            "profileURL": "https://www.facebook.com/profile.php?id=100051307060695"
+        },
+        {
+            "name": "Nhi Rex",
+            "profileURL": ""
+        },
+        {
+            "name": "Tiến Đạt",
+            "profileURL": "https://www.facebook.com/profile.php?id=100042140778354"
+        },
+        {
+            "name": "Nguyễn Thị Thanh Hoa",
+            "profileURL": "https://www.facebook.com/profile.php?id=100037528999692"
+        },
+        {
+            "name": "Vo Hoang Phi Nhung",
+            "profileURL": "https://www.facebook.com/ins.pnkunn"
+        },
+        {
+            "name": "Ngọc Trân",
+            "profileURL": "https://www.facebook.com/profile.php?id=100034553970517"
+        },
+        {
+            "name": "Ngô Khanh",
+            "profileURL": "https://www.facebook.com/ngo.h.khanh.716"
+        },
+        {
+            "name": "Quang Thiện",
+            "profileURL": "https://www.facebook.com/q.thien.vu.9"
+        },
+        {
+            "name": "Lê Trọng Nhân",
+            "profileURL": "https://www.facebook.com/profile.php?id=100027334579720"
+        },
+        {
+            "name": "Bùi Thanh Hiền",
+            "profileURL": "https://www.facebook.com/hien40555"
+        },
+        {
+            "name": "Kiều My",
+            "profileURL": "https://www.facebook.com/profile.php?id=100022277839522"
+        },
+        {
+            "name": "Thụy Ngân",
+            "profileURL": "https://www.facebook.com/ngandz100"
+        },
+        {
+            "name": "Lê Huy Hoàng",
+            "profileURL": "https://www.facebook.com/profile.php?id=100015905130912"
+        },
+        {
+            "name": "Nhật Hạ",
+            "profileURL": "https://www.facebook.com/nhatha941"
+        },
+        {
+            "name": "Tiến Lê",
+            "profileURL": "https://www.facebook.com/profile.php?id=100014569175965"
+        },
+        {
+            "name": "Minh Quân",
+            "profileURL": "https://www.facebook.com/quan31702"
+        },
+        {
+            "name": "Thanh Anh",
+            "profileURL": "https://www.facebook.com/thanhanh66"
+        },
+        {
+            "name": "Ân Thanh Tân",
+            "profileURL": "https://www.facebook.com/tandeptrai29122002"
+        },
+        {
+            "name": "Nguyễn AnhDuy",
+            "profileURL": "https://www.facebook.com/nguyenahdyy"
+        },
+        {
+            "name": "Nâu",
+            "profileURL": "https://www.facebook.com/naucuabame"
+        },
+        {
+            "name": "Namm Phuong",
+            "profileURL": "https://www.facebook.com/profile.php?id=61554253822537"
+        },
+        {
+            "name": "Hao Tu",
+            "profileURL": "https://www.facebook.com/profile.php?id=61552165441962"
+        },
+        {
+            "name": "Lê Đức Thảnh",
+            "profileURL": "https://www.facebook.com/profile.php?id=61551121340761"
+        }
+    ]
+][0];
 
 function compareFiles() {
     var fileA = document.getElementById('fileA').files[0];
@@ -279,6 +419,50 @@ function compareWithOriginal() {
 
     readerA.readAsText(fileA);
 }
+function compareWithOriginal2() {
+    var fileA = document.getElementById('fileA').files[0];
+    var readerA = new FileReader();
+
+    readerA.onload = function (e) {
+        var dataA;
+        try {
+            dataA = JSON.parse(e.target.result);
+            if (!Array.isArray(dataA)) {
+                throw new Error('Dữ liệu không phải là mảng.');
+            }
+        } catch (error) {
+            alert('Lỗi khi đọc dữ liệu từ File A: ' + error.message);
+            return;
+        }
+
+        var addedData = [];
+        var missingData = [];
+
+        fileOriginal2.forEach(function (itemOriginal) {
+            var existsInA = dataA.some(function (itemA) {
+                return itemOriginal.name === itemA.name;
+            });
+
+            if (!existsInA) {
+                missingData.push(itemOriginal);
+            }
+        });
+
+        dataA.forEach(function (itemA) {
+            var existsInOriginal = fileOriginal2.some(function (itemOriginal) {
+                return itemA.name === itemOriginal.name;
+            });
+
+            if (!existsInOriginal) {
+                addedData.push(itemA);
+            }
+        });
+
+        displayResult(addedData, missingData, fileOriginal2.length, dataA.length);
+    };
+
+    readerA.readAsText(fileA);
+}
 
 function compareWithFileB() {
     var fileA = document.getElementById('fileA').files[0];
@@ -333,7 +517,7 @@ function displayResult(addedData, missingData, totalDataA, totalDataB) {
     var totalAddedData = addedData.length;
     var totalMissingData = missingData.length;
 
-    resultContainer.innerHTML += '<p>Tổng số bạn bè ở cũ: ' + totalDataA + ' và mới là ' + totalDataB + '</p>';
+    resultContainer.innerHTML += '<p>Tổng số bạn bè File A:' + totalDataA + ' và File B:' + totalDataB + '</p>';
     resultContainer.innerHTML += '<p>' + totalAddedData + ' Bạn bè mới:</p>';
 
     if (addedData.length > 0) {
