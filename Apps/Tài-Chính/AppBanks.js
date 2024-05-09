722ed1aff4aee4789a6b6e6986b45a62dc763b90
Chesino <45505217+chesino@users.noreply.github.com>
2024-05-09 23:45:59 +0700

    Update AppBanks.js

--- Apps/Tài-Chính/AppBanks.js
+++ Apps/Tài-Chính/AppBanks.js
@@ -1,74 +1,94 @@
 function handlePasteClick() {
-    navigator.clipboard.read()
-        .then(function (clipboardItems) {
-            return clipboardItems[0].getType();
-        })
-        .then(function (clipboardType) {
-            if (clipboardType === 'text') {
-                return navigator.clipboard.readText();
-            } else {
-                throw new Error('Unsupported clipboard item type');
-            }
-        })
-        .then(function (clipboardData) {
-            alert("Đã nhấn 2");
-            const inputText = clipboardData;
-
-            const regexA = /[+-]?\d{1,3}(?:,\d{3})*(?:,\d{1,3})?(?= VND(?!\.))/;
-            const regexB = /\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}/;
-            const regexC = /\d{1,3}(?:,\d{3})*(?= VND\.)/;
-            const regexE = /toi\d{10} [A-Z\s]+|toi \d+ [A-Z\s]+/g;
-            const regexF = /(?<=\()[^)]+(?=\))/;
-            const regexD = /tu \d+ [A-Z\s]+/g;
-            const regexArr = [regexA, regexB, regexC, regexE, regexF, regexD];
-
-            const matches = regexArr.map(regex => inputText.match(regex));
-
-            let transactionMessage = '';
-
-            if (matches[0] !== null) {
-                let SoTienGD = parseFloat(matches[0][0].replace(/,/g, ''));
-                let SoTienGDr = matches[0][0];
-                let ThoiGianGD = '[' + matches[1][0].replace(/-/g, '/') + ']';
-
-                if (matches[3] !== null) {
-                    let NguoiNhan = matches[3][0].startsWith('toi') ? 'Chuyển tiền tới ' + matches[3][0].replace(/^toi | N$/g, '') : matches[3][0];
-                    let NguoiGui = matches[5][0].startsWith('tu') ? 'Từ ' + matches[5][0].replace(/^tu /, '') : matches[5][0];
-                    if (SoTienGD < 0) {
-                        transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + ' đ. ' + NguoiNhan + '.';
-                    } else {
-                        transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + ' đ. ' + NguoiGui + NguoiNhan + '.';
-                    }
-
-                } else {
-                    if (SoTienGD < 0) {
-                        transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + ' đ.';
-                    } else {
-                        transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + ' đ.';
-                    }
-                }
-
-                displayTransaction(transactionMessage);
-                transactionsHistory.unshift(transactionMessage);
-                SaveHistory();
-                displayTransactionHistory();
+  navigator.clipboard.readText()
+    .then(function(clipboardData) {
+      const inputText = clipboardData;
 
 
-                if (SoTienGD < 0) {
-                    addMoney(0, Number(SoTienGD));
-                    return;
-                }
-                if (SoTienGD > 0) {
-                    addMoney(0, Number(SoTienGD));
-                    return;
-                }
-            } else {
-                Fail('Không đúng định dạng hoặc không có dữ liệu.');
-            }
-        })
-        .catch(function (error) {
-            console.error('Lỗi: ', error);
-            Warning('🤔 Có sao chép gì đâu mà dán.');
-        });
-    Rule503020();
+      let regexA, regexB, regexC, regexD, regexE, regexF;
+
+
+      if (inputText.includes("Thẻ VCB Visa")) {
+        regexA = /[+-]?\d{1,3}(?:,\d{3})*(?:,\d{1,3})?(?= VND(?!\.))/;
+        regexB = /\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}/;
+        regexC = /\d{1,3}(?:,\d{3})*(?= VND\.)/;
+        regexE = /sử dụng tại ([^\d]+) số tiền/;
+      } else {
+        regexA = /[+-]?\d{1,3}(?:,\d{3})*(?:,\d{1,3})?(?= VND(?!\.))/;
+        regexB = /\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}/;
+        regexC = /\d{1,3}(?:,\d{3})*(?= VND\.)/;
+        regexE = /toi\d{10} [A-Z\s]+|toi \d+ [A-Z\s]+/g;
+        regexF = /\([^)]+\)/;
+        regexD = /tu \d+ [A-Z\s]+/g;
+      }
+
+
+      const regexArr = [regexA, regexB, regexC, regexE, regexF, regexD];
+
+      const matches = regexArr.map(regex => inputText.match(regex));
+
+      let transactionMessage = '';
+
+      if (matches[0] !== null) {
+        var SoTienGD = matches[0][0].replace(/,/g, '');
+        var SoTienGDr = matches[0][0];
+        var ThoiGianGD = '[' + matches[1][0].replace(/-/g, '/') + ']';
+
+        let NoteGD = "";
+        if (inputText.includes("Thẻ VCB Visa")) {
+          SoTienGD = "-" + SoTienGD;
+          SoTienGDr = "-" + SoTienGDr;
+          NoteGD = "SD tại..." ;
+        };
+        if (inputText.includes("Hủy GD thẻ VCB Visa")) {
+          SoTienGD = "+" + SoTienGD;
+          SoTienGDr = "+" + SoTienGDr;
+          NoteGD = "Hoàn trả do huỷ GD" ;
+        };
+        if (inputText.includes("MOMO")) {
+          NoteGD = "GD MOMO."
+
+        };
+
+
+
+
+        if (matches[3] !== null) {
+          var NguoiNhan = matches[3][0].replace('toi', 'Chuyển tiền tới ').replace(/^toi | N$/g, '');
+          var NguoiGui = matches[5][0].replace('tu', 'Từ ');
+          if (SoTienGD < 0) {
+            transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + ' đ. ' + NguoiNhan + '.';
+          } else {
+            transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + ' đ. ' + NguoiGui + NguoiNhan + '.';
+          }
+
+        } else {
+          if (SoTienGD < 0) {
+            transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + 'đ.' + NoteGD;
+          } else {
+            transactionMessage = ThoiGianGD + ' Số dư Tiền Thẻ ' + SoTienGDr + 'đ.' + NoteGD;
+          }
+        }
+
+        displayTransaction(transactionMessage);
+        transactionsHistory.unshift(transactionMessage);
+        SaveHistory();
+        displayTransactionHistory();
+
+
+        if (SoTienGD < 0) {
+          addMoney(0, Number(SoTienGD));
+          return;
+        }
+        if (SoTienGD > 0) {
+          addMoney(0, Number(SoTienGD));
+          return;
+        }
+      } else {
+        Fail('Không đúng định dạng hoặc không có dữ liệu.')
+      }
+    })
+    .catch(function() {
+      Warning('🤔 Có sao chép gì đâu mà dán.')
+    });
+  Rule503020();
 }
