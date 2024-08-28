cd9372d6453f6ad3463a30a0633765c601d0012c
Chesino <45505217+chesino@users.noreply.github.com>
2024-08-29 02:17:12 +0700

    Hunq

--- Apps/Lịch-Công-Giáo/DATA/Asset/Script.js
+++ Apps/Lịch-Công-Giáo/DATA/Asset/Script.js
@@ -12,18 +12,55 @@ document.addEventListener('DOMContentLoaded', function () {
                 const celebrationType = contentDiv.querySelector('.ploaile').innerText.trim();
                 const readingsAndGospel = contentDiv.querySelector('.dsbd').innerText.trim();
                 const priestVestmentColor = contentDiv.querySelector('.paole').innerText.trim();
-                const lcg = contentDiv.querySelector('.lcgtg:first-child').innerHTML.trim();
+                const lcg = contentDiv.querySelector('.lcgth').innerHTML.trim();
 
                 // Gán dữ liệu vào các thẻ p với id tương ứng
                 document.querySelector('.content').innerHTML = `
-                    <div class="main">
-                        <p id="date-saint">${dateAndSaint}</p>
-                        <p id="celebration-type">${celebrationType}</p>
-                        <p id="readings-gospel">${readingsAndGospel}</p>
-                        <p id="priest-vestment-color">${priestVestmentColor}</p>
-                    </div>
                     <div>${lcg}</div>
                 `;
+
+                const leDIV = document.getElementById('leDIV');
+                const ngayhomnay = document.getElementById('ngayhomnay');
+                const loaile = document.getElementById('loaile');
+                const lehomnay = document.getElementById('lehomnay');
+                const tinmung = document.getElementById('tinmung');
+                const aole = document.getElementById('aole');
+
+                const [date, content] = dateAndSaint.split(": ");
+                ngayhomnay.innerText = date;
+
+                if (celebrationType === 'Lễ nhớ - Thường Niên') {
+                    loaile.style.backgroundColor = '#ffff0099'
+                    loaile.style.color = '#000000'
+                } else if (celebrationType === 'Lễ trọng - Thường Niên') {
+                    loaile.style.backgroundColor = '#ff00009e'
+                    loaile.style.color = '#ffffff'
+                } else if (celebrationType === 'Lễ kính - Thường Niên') {
+                    loaile.style.backgroundColor = '#00fd1f8c'
+                    loaile.style.color = '#000000'
+                }
+                loaile.innerText = celebrationType;
+                lehomnay.innerText = content;
+
+                const readings = readingsAndGospel.replace(`Các bài đọc và tin mừng hôm nay`, "");
+                tinmung.innerText = readings;
+
+                const mauao = priestVestmentColor.replace("Áo Lễ Linh Mục: ", "");
+
+                if (mauao === 'Đỏ') {
+                    aole.src = './DATA/Chasuble/Red.png'
+                } else if (mauao === 'Xanh') {
+                    aole.src = './DATA/Chasuble/Green.png'
+                } else if (mauao === 'Trắng') {
+                    aole.src = './DATA/Chasuble/White.png'
+                } else if (mauao === 'Vàng') {
+                    aole.src = './DATA/Chasuble/Gold.png'
+                } else if (mauao === 'Hồng') {
+                    aole.src = './DATA/Chasuble/Pink.png'
+                } else if (mauao === 'Tím') {
+                    aole.src = './DATA/Chasuble/Purple.png'
+                }
+
             } else {
                 document.querySelector('.content').innerHTML = 'Nội dung không tìm thấy';
             }
